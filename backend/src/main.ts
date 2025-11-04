e97a49858e48eab65771a89a1a99211375008b8d
Zied Sebai <ziedsebai20@gmail.com>
2025-11-04 10:06:41 +0100

    cors bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 let cachedApp: express.Express;
 
@@ -10,14 +11,24 @@ async function createApp(): Promise<express.Express> {
   if (cachedApp) return cachedApp;
 
   const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-
-  // Enable CORS
   const allowedOrigins = [
     process.env.FRONT_BASE_URL,
     'https://trendy-shop-lu5s.vercel.app',
     'http://localhost:5173'
   ].filter(Boolean);
+  expressApp.use(
+    cors({
+      origin: allowedOrigins,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    })
+  );
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  // Enable CORS
+  
 
   app.enableCors({
     origin: allowedOrigins,
